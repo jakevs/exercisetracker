324e56c6e03870dba33c46b1a9467efa422cf607
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-07-18 00:15:08 -0500

    added GET functions to htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,17 @@
 const path = require("path");
 
 const router = require("express").Router();
+
+router.get("/", function (req, res) {
+    res.sendFile(path.join(_dirname, "../public/index.html"));
+});
+
+router.get("/exercise", function (req, res) {
+    res.sendFile(path.join(_dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+    res.sendFile(path.join(_dirname, "../public/stats.html"));
+});
+
+module.exports = router;
