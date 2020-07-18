1e167800e89256c3b71ce6346669b12ca21fa785
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-07-18 00:52:51 -0500

    added apiRoutes get, push and post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,43 @@
 const router = require("express").Router();
 const path = require("path");
+const Workout = require("../models/workout");
 
+router.post("/api/workouts", (req, res) => {
+    Workout.create({}).then(workout => {
+        res.json(workout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(params.id, {
+        $push: { exercises: body },
+    })
+    .then(workout => res.json(workout))
+    .catch(err => res.json(err));
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}, (err, stats) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(stats);
+      }
+    });
+  });
+
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({}, (err, stats) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(stats);
+    }
+  });
+});
+
+module.exports = router;
