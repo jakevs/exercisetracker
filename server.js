bc22a1bed565f6b2cd03d1bc159993174bd33900
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-07-17 23:09:03 -0500

    added to server file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.use(htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`App is listening on ${PORT}.`);
+});
