602d07d2e9fea2dfc00cfa56506691b6106e02ec
James Van Slyke <60765773+jakevs@users.noreply.github.com>
2020-07-19 21:43:26 -0500

    added htmlRoutes and exporting workout

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,58 @@
 const mogoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 // const Cat = mongoose.model('Cat', { name: String });
 
 // const kitty = new Cat({ name: 'Zildjian' });
 // kitty.save().then(() => console.log('meow'));
+
+const Schema = mongoose.Schema;
+
+const workOutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [
+            {
+            type: {
+                type: String,
+                trim: true,
+                required: "This field is required",
+                },
+            name: {
+                type: String,
+                trim: true,
+                required: "This field is required",
+                },
+            duration: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            weight: {
+                type: Number
+            }
+    }]},
+    {
+        toJson: {
+            virtuals: true,
+        }});
+
+workOutSchema.virtual("totalDuration").get(function () {
+      return this.exercises.reduce((total, exercises) => {
+      return total + exercises.duration;
+      }, 0);
+      });
+          
+const Workout = mongoose.model("Workout", workOutSchema);
+          
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3000;
 
